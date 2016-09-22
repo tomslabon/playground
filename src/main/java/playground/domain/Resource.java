fa27a0f46edd99fd9cdca5bdb06a8f8f518fc404
tomslabon <tomslabon@gmail.com>
2016-09-22 13:34:54 +0200

    First code commit

---
+++ src/main/java/playground/domain/Resource.java
@@ -0,0 +1,13 @@
+package playground.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+public class Resource {
+
+	@Getter @Setter
+	private String name;
+	
+}
---
+++ src/test/java/playground/domain/ResourceTest.java
@@ -0,0 +1,13 @@
+package playground.domain;
+
+import org.junit.Test;
+
+public class ResourceTest {
+
+	@Test
+	public void isAccessorAvailable() {
+		Resource resource = new Resource();
+		resource.getName();
+	}
+	
+}
